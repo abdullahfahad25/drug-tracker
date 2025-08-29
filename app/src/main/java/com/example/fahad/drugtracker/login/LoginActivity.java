fe991e9eec86062b16dfe8c6c1c6c5405290765d
Md. Abdullah Al Fahad Bhuiyan <md.a.fahad25@gmail.com>
2025-08-30 00:07:05 +0600

    Initialized with project structure

---
+++ app/src/main/java/com/example/fahad/drugtracker/login/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.fahad.drugtracker.login;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.fahad.drugtracker.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

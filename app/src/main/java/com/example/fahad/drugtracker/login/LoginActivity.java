430cbc129e56d74c449aa6460b541dce7996f3bb
Md. Abdullah Al Fahad Bhuiyan <md.a.fahad25@gmail.com>
2025-08-31 00:56:08 +0600

    Added Firebase Auth support for Signup/login

--- app/src/main/java/com/example/fahad/drugtracker/login/LoginActivity.java
+++ app/src/main/java/com/example/fahad/drugtracker/login/LoginActivity.java
@@ -1,17 +1,25 @@
 package com.example.fahad.drugtracker.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.fahad.drugtracker.R;
+import com.example.fahad.drugtracker.user.HomeActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
@@ -19,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText email;
     private EditText password;
     private Button btnLogin;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +39,32 @@ public class LoginActivity extends AppCompatActivity {
         email = findViewById(R.id.emailLogin);
         password = findViewById(R.id.passwordLogin);
         btnLogin = findViewById(R.id.loginBtn);
+        auth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(view -> doLogin());
+    }
+
+    private void doLogin() {
+        String email = this.email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        if (email.isEmpty() || pass.isEmpty()) {
+            Toast.makeText(this, "Fill fields", Toast.LENGTH_SHORT).show(); return;
+        }
+
+
+        auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Auth failed: "
+                                    + (task.getException() != null ? task.getException().getMessage() : "")
+                                    , Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/com/example/fahad/drugtracker/signup/SignupActivity.java
+++ app/src/main/java/com/example/fahad/drugtracker/signup/SignupActivity.java
@@ -1,17 +1,26 @@
 package com.example.fahad.drugtracker.signup;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.fahad.drugtracker.R;
+import com.example.fahad.drugtracker.login.LoginActivity;
+import com.example.fahad.drugtracker.user.HomeActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
     private static final String TAG = "SignupActivity";
@@ -20,6 +29,7 @@ public class SignupActivity extends AppCompatActivity {
     private EditText email;
     private EditText password;
     private Button btnSignup;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,5 +42,33 @@ public class SignupActivity extends AppCompatActivity {
         email = findViewById(R.id.emailSignup);
         password = findViewById(R.id.passwordSignup);
         btnSignup = findViewById(R.id.signupBtn);
+        auth = FirebaseAuth.getInstance();
+
+        btnSignup.setOnClickListener(view -> doSignup());
+    }
+
+    private void doSignup() {
+        String email = this.email.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        String name = this.name.getText().toString().trim();
+        if (email.isEmpty() || pass.isEmpty() || name.isEmpty()) {
+            Toast.makeText(this, "Fill fields", Toast.LENGTH_SHORT).show(); return;
+        }
+
+
+        auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                            finish();
+                        } else {
+                            Toast.makeText(SignupActivity.this, "Signup failed: "
+                                            + (task.getException() != null ? task.getException().getMessage() : "")
+                                    , Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
     }
 }

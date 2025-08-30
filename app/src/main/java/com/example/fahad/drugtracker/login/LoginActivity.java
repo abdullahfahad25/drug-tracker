41b32f639038f10be720fe4ad4dc020f6409afc1
Md. Abdullah Al Fahad Bhuiyan <md.a.fahad25@gmail.com>
2025-08-30 12:29:07 +0600

    Added basic layout for login and signup activity

--- app/src/main/java/com/example/fahad/drugtracker/login/LoginActivity.java
+++ app/src/main/java/com/example/fahad/drugtracker/login/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.fahad.drugtracker.login;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +14,21 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.fahad.drugtracker.R;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+
+    private EditText email;
+    private EditText password;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.emailLogin);
+        password = findViewById(R.id.passwordLogin);
+        btnLogin = findViewById(R.id.loginBtn);
     }
 }
--- app/src/main/java/com/example/fahad/drugtracker/signup/SignupActivity.java
+++ app/src/main/java/com/example/fahad/drugtracker/signup/SignupActivity.java
@@ -1,6 +1,9 @@
 package com.example.fahad.drugtracker.signup;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +14,23 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.fahad.drugtracker.R;
 
 public class SignupActivity extends AppCompatActivity {
+    private static final String TAG = "SignupActivity";
+
+    private EditText name;
+    private EditText email;
+    private EditText password;
+    private Button btnSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        name = findViewById(R.id.nameSignup);
+        email = findViewById(R.id.emailSignup);
+        password = findViewById(R.id.passwordSignup);
+        btnSignup = findViewById(R.id.signupBtn);
     }
 }

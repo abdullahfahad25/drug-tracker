0961d75939f87518f2cb334043a56440bccb34fc
Md. Abdullah Al Fahad Bhuiyan <md.a.fahad25@gmail.com>
2025-08-31 13:56:25 +0600

    Added fragment transaction in HomeActivity

--- app/src/main/java/com/example/fahad/drugtracker/user/HomeActivity.java
+++ app/src/main/java/com/example/fahad/drugtracker/user/HomeActivity.java
@@ -1,14 +1,19 @@
 package com.example.fahad.drugtracker.user;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.fahad.drugtracker.R;
+import com.example.fahad.drugtracker.user.search.ui.SearchFragment;
+import com.example.fahad.drugtracker.user.userDrugs.ui.UserDrugsFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -16,5 +21,34 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // Load TrackerFragment by default
+        if (savedInstanceState == null) {
+            loadFragment(new UserDrugsFragment(), false);
+        }
+
+        // Open SearchFragment when button clicked
+        Button btnOpenSearch = findViewById(R.id.btnOpenSearch);
+        btnOpenSearch.setOnClickListener(v -> {
+            loadFragment(new SearchFragment(), true);
+        });
+    }
+
+    public void loadFragment(Fragment fragment, boolean addToBackStack) {
+        FragmentTransaction transaction = getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment);
+
+        if (addToBackStack) {
+            transaction.addToBackStack(null);
+        }
+
+        transaction.commit();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        getSupportFragmentManager().popBackStack();
+        return true;
     }
 }

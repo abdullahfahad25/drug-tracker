de4698ed1cd2de24d9529547137b21401bd3e34c
Md. Abdullah Al Fahad Bhuiyan <md.a.fahad25@gmail.com>
2025-08-31 13:41:02 +0600

    Added clicklisteners for MainActivity

--- app/src/main/java/com/example/fahad/drugtracker/MainActivity.java
+++ app/src/main/java/com/example/fahad/drugtracker/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.fahad.drugtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +11,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.fahad.drugtracker.login.LoginActivity;
+import com.example.fahad.drugtracker.signup.SignupActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TextView alreadyAcc;
+    private Button createAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        alreadyAcc = findViewById(R.id.alreadyAcc);
+        createAcc = findViewById(R.id.newAcc);
+
+        setupClickListeners();
+    }
+
+    private void setupClickListeners() {
+        alreadyAcc.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        createAcc.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+            startActivity(intent);
+        });
     }
 }
--- app/src/main/java/com/example/fahad/drugtracker/user/userDrugs/ui/UserDrugsFragment.java
+++ app/src/main/java/com/example/fahad/drugtracker/user/userDrugs/ui/UserDrugsFragment.java
@@ -69,7 +69,7 @@ public class UserDrugsFragment extends Fragment {
                 viewModel.deleteDrug(drug);
             }
         };
-        
+
         new ItemTouchHelper(simpleCallback).attachToRecyclerView(recyclerView);
     }
 
